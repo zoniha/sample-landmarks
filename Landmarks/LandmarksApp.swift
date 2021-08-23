72d3e99e0a868ed829d6d7fd48c1fa3ef9ccc290
zoniha <awaawapurin@gmail.com>
2021-08-23 18:22:43 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by zoniha on 2021/08/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
