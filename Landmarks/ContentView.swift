b57a65f9f5647327a8688e5799615b030d3d0496
zoniha <awaawapurin@gmail.com>
2021-08-23 19:47:24 +0900

    🎉 init: Initial commit

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -1,21 +1,24 @@
-//
-//  ContentView.swift
-//  Landmarks
-//
-//  Created by zoniha on 2021/08/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+	var body: some View {
+		VStack(alignment: .leading) {
+			Text("Turtle Rock")
+				.font(.title)
+			HStack {
+				Text("Joshua Tree National Park")
+					.font(.subheadline)
+				Spacer()
+				Text("California")
+					.font(.subheadline)
+			}
+		}
+		.padding()
+	}
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+	static var previews: some View {
+		ContentView()
+	}
 }
