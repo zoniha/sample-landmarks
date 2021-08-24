2d7273746b9de30fbc1f4468858b6819cb703a2f
zoniha <awaawapurin@gmail.com>
2021-08-25 03:14:41 +0900

    ✨ feat: Complete landmarks

--- Landmarks/Views/CircleImage.swift
+++ Landmarks/Views/CircleImage.swift
@@ -5,16 +5,18 @@ struct CircleImage: View {
 	let gRate = 199 / 255.0
 	let bRate = 232 / 255.0
 
-    var body: some View {
-        Image("turtlerock")
+	var image: Image
+
+	var body: some View {
+		image
 			.clipShape(Circle())
 			.overlay(Circle().stroke(Color(red: rRate, green: gRate, blue: bRate, opacity: 1.0), lineWidth: 4))
 			.shadow(radius: 7)
-    }
+	}
 }
 
 struct CircleImage_Previews: PreviewProvider {
-    static var previews: some View {
-        CircleImage()
-    }
+	static var previews: some View {
+		CircleImage(image: Image("turtlerock"))
+	}
 }
