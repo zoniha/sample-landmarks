b1de87f407788a35f5d09b529b0950c747b09fb5
zoniha <awaawapurin@gmail.com>
2021-08-24 14:34:17 +0900

    🎉 init: Initial commit

---
+++ Landmarks/Views/MapView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text("")
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
